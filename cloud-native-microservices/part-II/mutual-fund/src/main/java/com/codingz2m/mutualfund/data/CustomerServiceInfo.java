dce998f5849a88d2d78892b41954da1cea919843
CODINGZ2M <41149999+codingZ2M@users.noreply.github.com>
2022-11-18 11:49:47 +0530

    Update CustomerServiceInfo.java

--- cloud-native-microservices/part-II/mutual-fund/src/main/java/com/codingz2m/mutualfund/data/CustomerServiceInfo.java
+++ cloud-native-microservices/part-II/mutual-fund/src/main/java/com/codingz2m/mutualfund/data/CustomerServiceInfo.java
@@ -1,6 +1,5 @@
 package com.codingz2m.mutualfund.data;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +7,6 @@ import lombok.ToString;
 
 @Component
 @Getter @Setter @ToString
-@ConfigurationProperties(prefix="mutualfund")
 public class CustomerServiceInfo {
 	
 	 private String message;
