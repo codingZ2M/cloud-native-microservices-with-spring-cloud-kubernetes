642b4d85203fa17ff30361bf3298280477634453
CODINGZ2M <41149999+codingZ2M@users.noreply.github.com>
2022-11-18 11:47:12 +0530

    Update MutualFundServiceConfig.java

--- cloud-native-microservices/part-II/mutual-fund/src/main/java/com/codingz2m/mutualfund/config/MutualFundServiceConfig.java
+++ cloud-native-microservices/part-II/mutual-fund/src/main/java/com/codingz2m/mutualfund/config/MutualFundServiceConfig.java
@@ -3,13 +3,13 @@ package com.codingz2m.mutualfund.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Component
+@Configuration
 //Loading all properties from the given file
 //Once the particular property file loaded, reading all the properties with prefix value of "mutualfund"
 @ConfigurationProperties(prefix = "mutualfund")
