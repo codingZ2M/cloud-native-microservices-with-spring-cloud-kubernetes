0b8452ceeea72abfc5f2c10fdc42929fa8ff407d
CODINGZ2M <41149999+codingZ2M@users.noreply.github.com>
2022-11-23 15:43:49 +0530

    Add files via upload

---
+++ cloud-native-microservices/part-IV/mutual-fund/src/main/java/com/codingz2m/mutualfund/config/MutualFundServiceConfig.java
@@ -0,0 +1,27 @@
+package com.codingz2m.mutualfund.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Configuration
+//Loading all properties from the given file
+//Once the particular property file loaded, reading all the properties with prefix value of "mutualfund"
+@ConfigurationProperties(prefix = "mutualfund")
+@Getter @Setter @ToString
+public class MutualFundServiceConfig {
+	
+	 private String message;
+	 private String mail;
+	 private String contact;
+	 
+	 @Bean
+	 public ModelMapper getModelMapper() {
+		return new ModelMapper(); 
+	 }
+}
