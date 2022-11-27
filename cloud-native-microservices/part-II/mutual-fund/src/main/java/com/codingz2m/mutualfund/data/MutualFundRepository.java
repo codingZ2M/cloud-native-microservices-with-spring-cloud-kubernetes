81a4b7e7cb40861a16464af2b2c3975ea435f246
CODINGZ2M <41149999+codingZ2M@users.noreply.github.com>
2022-11-27 21:33:56 +0530

    Delete cloud-native-microservices/part-II/mutual-fund directory

--- cloud-native-microservices/part-II/mutual-fund/src/main/java/com/codingz2m/mutualfund/data/MutualFundRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codingz2m.mutualfund.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface MutualFundRepository extends CrudRepository<MutualFund, Long> {
-
-}
-
