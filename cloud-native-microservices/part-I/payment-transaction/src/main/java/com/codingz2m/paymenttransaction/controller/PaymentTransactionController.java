a697dad9adf942675de123b20360203ea3e547c3
CODINGZ2M <41149999+codingZ2M@users.noreply.github.com>
2022-11-27 19:27:23 +0530

    Add files via upload

---
+++ cloud-native-microservices/part-I/payment-transaction/src/main/java/com/codingz2m/paymenttransaction/controller/PaymentTransactionController.java
@@ -0,0 +1,72 @@
+package com.codingz2m.paymenttransaction.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codingz2m.paymenttransaction.repository.PaymentTransaction;
+import com.codingz2m.paymenttransaction.service.PaymentTransactionService;
+import com.codingz2m.paymenttransaction.shared.PaymentTransactionDTO;
+import com.codingz2m.paymenttransaction.ui.model.PaymentTransactionRequest;
+import com.codingz2m.paymenttransaction.ui.model.PaymentTransactionResponse;
+
+
+@RestController
+@RequestMapping("/payment-transaction")
+public class PaymentTransactionController {
+
+	private static final Logger logger = LoggerFactory.getLogger(PaymentTransactionController.class);
+	
+	private PaymentTransactionService paymentTransactionService;
+	@Autowired	
+	ModelMapper modelMapper;
+	
+	@Autowired	
+	public PaymentTransactionController(PaymentTransactionService paymentTransactionService) {
+		this.paymentTransactionService = paymentTransactionService;
+	}
+	
+	@PostMapping
+	public void initiatePaymentTransaction( @RequestBody PaymentTransactionRequest paymentTransactionRequest
+			) {
+		logger.info("initiatePaymentTransaction() method in payment-transaction service started");
+		//PaymentTransactionDTO paymentTransactionDTO = new PaymentTransactionDTO();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		PaymentTransactionDTO paymentTransactionDTO = modelMapper.map(paymentTransactionRequest, PaymentTransactionDTO.class);
+		
+	    paymentTransactionService.paymentTransaction(paymentTransactionDTO);
+	    logger.info("initiatePaymentTransaction() method in payment-transaction service ended");
+	}
+	
+	
+	// HTTP GET with Spring MVC
+    // One-To-Many Mapping (BI-Directional: From  MutualFundPortfolio To List <MutualFund> Object)
+	  @GetMapping(path ="/{savings-account-id}")
+	    public List<PaymentTransactionResponse > getPaymentTransactions(
+	    		@PathVariable(value="savings-account-id") Long savingsAccountId){
+		  
+		  List <PaymentTransaction> paymentTransactionList   = new ArrayList<>();
+		  List <PaymentTransactionResponse> paymentTransactionResponseList   = new ArrayList<>();
+		  
+			modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+			
+			paymentTransactionList = paymentTransactionService.getPaymentTransactions(savingsAccountId);
+			  
+			 paymentTransactionResponseList = Arrays.asList( modelMapper.map(paymentTransactionList, PaymentTransactionResponse[].class));
+			return  paymentTransactionResponseList;
+			
+	    }
+}
