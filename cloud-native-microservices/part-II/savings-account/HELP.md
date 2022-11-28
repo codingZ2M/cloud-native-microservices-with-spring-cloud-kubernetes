8d9c6a4629d1fb3c9d45560e9c88610d06d6c6c6
CODINGZ2M <41149999+codingZ2M@users.noreply.github.com>
2022-11-28 10:57:50 +0530

    Add files via upload

---
+++ cloud-native-microservices/part-II/savings-account/HELP.md
@@ -0,0 +1,36 @@
+# Read Me First
+The following was discovered as part of building this project:
+
+* The original package name 'com.codingz2m.savings-account' is invalid and this project uses 'com.codingz2m.savingsaccount' instead.
+
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/3.0.0/maven-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/3.0.0/maven-plugin/reference/html/#build-image)
+* [Spring Boot DevTools](https://docs.spring.io/spring-boot/docs/3.0.0/reference/htmlsingle/#using.devtools)
+* [Spring Boot Actuator](https://docs.spring.io/spring-boot/docs/3.0.0/reference/htmlsingle/#actuator)
+* [Config Client Quick Start](https://docs.spring.io/spring-cloud-config/docs/current/reference/html/#_client_side_usage)
+* [Spring Data JPA](https://docs.spring.io/spring-boot/docs/3.0.0/reference/htmlsingle/#data.sql.jpa-and-spring-data)
+* [Spring HATEOAS](https://docs.spring.io/spring-boot/docs/3.0.0/reference/htmlsingle/#web.spring-hateoas)
+* [Spring Web](https://docs.spring.io/spring-boot/docs/3.0.0/reference/htmlsingle/#web)
+* [OpenFeign](https://docs.spring.io/spring-cloud-openfeign/docs/current/reference/html/)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Building a RESTful Web Service with Spring Boot Actuator](https://spring.io/guides/gs/actuator-service/)
+* [Accessing Data with JPA](https://spring.io/guides/gs/accessing-data-jpa/)
+* [Building a Hypermedia-Driven RESTful Web Service](https://spring.io/guides/gs/rest-hateoas/)
+* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
+* [Building REST services with Spring](https://spring.io/guides/tutorials/rest/)
+
+### Additional Links
+These additional references should also help you:
+
+* [Declarative REST calls with Spring Cloud OpenFeign sample](https://github.com/spring-cloud-samples/feign-eureka)
+
---
+++ cloud-native-microservices/part-II/savings-account/src/main/java/com/codingz2m/savingsaccount/service/SavingsAccountServiceImpl.java
@@ -0,0 +1,91 @@
+package com.codingz2m.savingsaccount.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.codingz2m.savingsaccount.repository.SavingsAccount;
+import com.codingz2m.savingsaccount.repository.SavingsAccountRepository;
+import com.codingz2m.savingsaccount.shared.SavingsAccountDTO;
+import com.codingz2m.savingsaccount.ui.models.PaymentTransactionRequest;
+
+import jakarta.transaction.Transactional;
+
+@Service(value="savingsAccount")
+@Transactional 
+public class SavingsAccountServiceImpl implements SavingsAccountService {
+
+	SavingsAccountRepository  savingsAccountRepository;
+	
+	@Autowired
+	public SavingsAccountServiceImpl(SavingsAccountRepository savingsAccountRepository) {
+		super();
+		this.savingsAccountRepository = savingsAccountRepository;
+	}
+
+	@Override
+	public SavingsAccount createSavingsAccount(SavingsAccountDTO savingsAccountDTO) {
+		 ModelMapper modelMapper = new ModelMapper(); 
+		  modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		  
+		  SavingsAccount savingsAccount = modelMapper.map(savingsAccountDTO, SavingsAccount.class);
+		  savingsAccount = savingsAccountRepository.save(savingsAccount);
+		  
+		return savingsAccount;
+	}
+
+	
+	@Override
+	public List<SavingsAccount> getSavingsAccounts() {
+		List <SavingsAccount> savingsAccountList   = new ArrayList<>();
+		savingsAccountList = (List<SavingsAccount>) savingsAccountRepository.findAll();
+		return savingsAccountList;
+	}
+	
+	
+	@Override
+	public SavingsAccount findAccountById(Long id) {
+		Optional<SavingsAccount> savingsAccount = savingsAccountRepository.findById(id);
+		return savingsAccount.get();
+	}
+
+	
+	@Override
+	public boolean mutualFundTransaction(Long savingsAccountId, double mutualFundAmonunt) {
+		boolean fundTransactionState;
+		double totalCurrentValue = savingsAccountRepository.getSavingsAccountCurrentValue(savingsAccountId);
+		System.out.println(totalCurrentValue);
+		if (totalCurrentValue > mutualFundAmonunt) {
+			totalCurrentValue -= mutualFundAmonunt;
+			fundTransactionState = true;
+		  }
+		else {
+			fundTransactionState = false;
+		 }		
+		savingsAccountRepository.updateSavingsAccount(savingsAccountId, totalCurrentValue);
+		return fundTransactionState;
+	}
+
+	@Override
+	public int savingsAccountTransaction(PaymentTransactionRequest savingsAccountTransactionRequest) {
+		
+		Long savingsAccountId = savingsAccountTransactionRequest.getSavingsAccountId();
+			double totalCurrentValue = savingsAccountRepository.getSavingsAccountCurrentValue(savingsAccountId);
+			double amountToBeDeposited =  savingsAccountTransactionRequest.getAmount();
+			String transactionType = savingsAccountTransactionRequest.getDebitOrCredit();
+			if (transactionType.equals("Credit"))
+				totalCurrentValue += amountToBeDeposited;
+			else
+				totalCurrentValue -= amountToBeDeposited;   
+		 int transactionCount = savingsAccountRepository.updateSavingsAccount(savingsAccountId, totalCurrentValue);
+	
+		 return transactionCount;
+	}
+
+
+}
