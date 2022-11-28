2ca241be50980f891d606df2fb0e176797a1f3bf
CODINGZ2M <41149999+codingZ2M@users.noreply.github.com>
2022-11-28 14:54:41 +0530

    Add files via upload

---
+++ cloud-native-microservices/part-III/payment-transaction/src/main/java/com/codingz2m/paymenttransaction/PaymentTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.codingz2m.paymenttransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentTransactionApplication.class, args);
+	}
+
+}
